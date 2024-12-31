a0b17f9a808c2d602cf0d4c78822202f23514ab0
freekvermaas <freekvermaas@gmail.com>
2024-12-31 01:20:05 +0100

    text dikgedrukt en positionering header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 const Header = ({ activeSection }) => {
+    const isActive = (section) =>
+      activeSection === section ? "font-bold" : "font-normal";
+  
     const textColor = activeSection === "about" ? "text-white" : "text-black";
     const lineColor = activeSection === "about" ? "bg-white" : "bg-black";
   
@@ -15,26 +18,29 @@ const Header = ({ activeSection }) => {
   
     return (
       <header className="fixed top-0 left-0 w-full pt-4">
-        <nav className="flex justify-between max-w-4xl mx-auto relative">
+        <nav className="flex justify-between items-center max-w-4xl mx-auto relative">
           <div
             className={`absolute top-0 left-0 right-0 h-px ${lineColor}`}
             aria-hidden="true"
           />
           <button
             onClick={() => handleScrollToSection("hero")}
-            className={`pt-2 px-4 ${textColor} focus:outline-none`}
+            className={`pt-2 text-center ${textColor} ${isActive("hero")} focus:outline-none`}
+            style={{ flex: "1 1 0%", textAlign: "start" }}
           >
             HOME
           </button>
           <button
             onClick={() => handleScrollToSection("about")}
-            className={`pt-2 px-4 ${textColor} focus:outline-none`}
+            className={`pt-2 px-4 ${textColor} ${isActive("about")} focus:outline-none`}
+            style={{ flex: "1 1 auto", textAlign: "center" }}
           >
             FREEK
           </button>
           <button
             onClick={() => handleScrollToSection("gallery")}
-            className={`pt-2 px-4 ${textColor} focus:outline-none`}
+            className={`pt-2 text-center ${textColor} ${isActive("gallery")} focus:outline-none`}
+            style={{ flex: "1 1 0%", textAlign: "end" }}
           >
             GALLERY
           </button>
