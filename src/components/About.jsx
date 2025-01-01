9c467926fac62b46afd60671de73d3300e8b2425
freekvermaas <freekvermaas@gmail.com>
2025-01-01 23:38:29 +0100

    london skyline image gevoegd + position

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,12 @@ import { memo } from "react"
 
 const About = memo(function About() {
   return (
-    (<div
-      className="flex-shrink-0 w-screen h-screen snap-start bg-black text-slate-50 p-8 flex flex-col justify-center items-center">
-    </div>)
+    <div className="flex-shrink-0 w-screen h-screen snap-start bg-black text-slate-50 p-8 flex flex-col justify-center items-start">
+      <div className="mb-20 md:mb-40 lg:mb-60">
+        <img src="/images/personalscenery3.png" alt="Personal Scenery" className="max-w-full h-auto ml-4 md:ml-8 lg:ml-16 w-1/2 sm:w-3/4 md:w-1/2 lg:w-1/3" />
+      </div>
+    </div>
   );
 })
 
 export default About
-
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { memo } from "react";
 const Hero = memo(function Hero() {
   return (
     <div className="flex-shrink-0 w-screen h-screen snap-start bg-white text-slate-50 p-8 flex flex-col justify-center items-start">
-      <div className="mb-10 md:mb-20 lg:mb-30">
-        <h1 className="text-black text-base font-extralight font-inter ml-4 md:ml-8 lg:ml-16">
+      <div className="mb-20 md:mb-40 lg:mb-60">
+        <h1 className="text-black text-base font-extralight font-idnter ml-4 md:ml-8 lg:ml-16">
           Portfolio
         </h1>
         <h2 className="text-black text-4xl md:text-5xl font-inter ml-4 md:ml-8 lg:ml-16">
