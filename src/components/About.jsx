cece8881e56e36cfc74d355b6c61f68f3440247f
freekvermaas <freekvermaas@gmail.com>
2025-01-01 23:53:01 +0100

    paragraaf onder london scenery

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,28 @@
-import { memo } from "react"
+import { memo } from "react";
 
 const About = memo(function About() {
   return (
     <div className="flex-shrink-0 w-screen h-screen snap-start bg-black text-slate-50 p-8 flex flex-col justify-center items-start">
-      <div className="mb-20 md:mb-40 lg:mb-60">
-        <img src="/images/personalscenery3.png" alt="Personal Scenery" className="max-w-full h-auto ml-4 md:ml-8 lg:ml-16 w-1/2 sm:w-3/4 md:w-1/2 lg:w-1/3" />
+      <div className="mt-1 md:mt-2 lg:mt-4">
+        <img
+          src="/images/personalscenery3.png"
+          alt="Personal Scenery"
+          className="max-w-full h-auto ml-4 md:ml-8 lg:ml-16 w-1/2 sm:w-3/4 md:w-1/2 lg:w-1/3"
+        />
+        <p className="text-white text-base font-inter font-light ml-4 md:ml-8 lg:ml-16 mt-8">
+        Mijn naam is Freek Vermaas, een 19-jarige UX/UI designer uit Tilburg <br></br>
+        met een sterke passie voor innovatie en creativiteit. Ik ben zeer<br></br>
+        ervaren met Adobe Photoshop en Figma, maar kan ook werken met<br></br>
+        andere hulptools zoals Adobe Illustrator en Premiere Pro. Ook kan ik<br></br>
+        goed op weg met AI tools als ChatGPT, Midjourney en Ideogram.<br></br>
+        <br></br>
+        Ik ben op zoek naar mogelijkheden binnen dit werkgebied om mijn<br></br>
+        skills te verbeteren, mijn kennis te vergroten en mijn<br></br>
+        creativiteit en inzicht tot het uiterste te testen.
+        </p>
       </div>
     </div>
   );
-})
+});
 
-export default About
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = memo(function Hero() {
   return (
     <div className="flex-shrink-0 w-screen h-screen snap-start bg-white text-slate-50 p-8 flex flex-col justify-center items-start">
       <div className="mb-20 md:mb-40 lg:mb-60">
-        <h1 className="text-black text-base font-extralight font-idnter ml-4 md:ml-8 lg:ml-16">
+        <h1 className="text-black text-base font-extralight font-inter ml-4 md:ml-8 lg:ml-16">
           Portfolio
         </h1>
         <h2 className="text-black text-4xl md:text-5xl font-inter ml-4 md:ml-8 lg:ml-16">
