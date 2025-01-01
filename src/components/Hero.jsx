e7fe30f6594b40d19fa53c16a592083dc4d2d1b2
freekvermaas <freekvermaas@gmail.com>
2025-01-01 23:18:56 +0100

    rainbow line image toegevoegd + positionering

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,14 @@ import { memo } from "react";
 const Hero = memo(function Hero() {
   return (
     <div className="flex-shrink-0 w-screen h-screen snap-start bg-white text-slate-50 p-8 flex flex-col justify-center items-start">
-      <div>
+      <div className="mb-10 md:mb-20 lg:mb-30">
         <h1 className="text-black text-base font-extralight font-inter ml-4 md:ml-8 lg:ml-16">
           Portfolio
         </h1>
         <h2 className="text-black text-4xl md:text-5xl font-inter ml-4 md:ml-8 lg:ml-16">
           <span className="font-bold">FREEK</span> <span className="font-extralight italic">VERMAAS</span>
         </h2>
+        <img src="/images/code1.png" className="w-full h-auto mt-1 ml-0.5 md:ml-1 lg:ml-2"/>
       </div>
     </div>
   );
