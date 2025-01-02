24db2c1e76e42ef1a3d24241cd66811edbdf9aec
freekvermaas <freekvermaas@gmail.com>
2025-01-02 13:03:20 +0100

    trendfocus image met hover gevoegd

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Gallery = memo(function Gallery() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [hoveredImage, setHoveredImage] = useState(null);
 
   return (
     <div className="w-screen h-screen snap-start bg-white text-slate-950 overflow-hidden flex flex-col">
@@ -14,16 +14,39 @@ const Gallery = memo(function Gallery() {
           Gallery
         </h1>
       </div>
-      <div className="flex-grow flex justify-center items-start pt-10">
-        <Link href="/destination-page">
+      <div className="flex-grow flex flex-col justify-start items-center pt-10">
+        <Link href="/destination-page-1">
           <div
             className="relative transition-transform duration-300 ease-in-out hover:scale-105"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
+            onMouseEnter={() => setHoveredImage(1)}
+            onMouseLeave={() => setHoveredImage(null)}
           >
             <Image
-              src={isHovered ? "/images/post_upendo_showcase.png" : "/images/pre_upendo_showcase.png"}
+              src={
+                hoveredImage === 1
+                  ? "/images/post_upendo_showcase.png"
+                  : "/images/pre_upendo_showcase.png"
+              }
               alt="Upendo Showcase"
+              width={300}
+              height={300}
+              className="w-[300px] h-[300px] object-contain"
+            />
+          </div>
+        </Link>
+        <Link href="/destination-page-2">
+          <div
+            className="relative transition-transform duration-300 ease-in-out hover:scale-105"
+            onMouseEnter={() => setHoveredImage(2)}
+            onMouseLeave={() => setHoveredImage(null)}
+          >
+            <Image
+              src={
+                hoveredImage === 2
+                  ? "/images/post_trendfocus_showcase.png"
+                  : "/images/pre_trendfocus_showcase.png"
+              }
+              alt="Another Showcase"
               width={300}
               height={300}
               className="w-[300px] h-[300px] object-contain"
@@ -36,4 +59,3 @@ const Gallery = memo(function Gallery() {
 });
 
 export default Gallery;
-
