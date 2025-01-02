420312513b160a609d3f33a3e92796bbe0bd9104
freekvermaas <freekvermaas@gmail.com>
2025-01-02 02:30:18 +0100

    bijna width fix homepagina image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,6 @@ import { memo } from "react";
 const Hero = memo(function Hero() {
   return (
     <div className="flex-shrink-0 w-screen h-screen snap-start bg-white text-slate-50 p-8 flex flex-col justify-center items-start pt-20">
-      {/* Header space to avoid overlap */}
       <div className="mb-20 md:mb-40 lg:mb-60">
         <h1 className="text-black text-base font-extralight font-inter ml-4 md:ml-8 lg:ml-16">
           Portfolio
@@ -17,7 +16,6 @@ const Hero = memo(function Hero() {
             src="/images/code1.png"
             className="w-full h-auto mt-1 ml-0.5 md:ml-1 lg:ml-2"
           />
-          {/* Second image positioned underneath with spacing on smaller screens */}
           <img
             src="/images/tempimage1.png"
             className="absolute top-full left-0 w-full h-auto mt-4 ml-4 md:ml-8 lg:ml-16"
