cd25900117f68ce29a31b6fdd7a91561a267e091
freekvermaas <freekvermaas@gmail.com>
2025-01-02 12:21:45 +0100

    start van gallery pagina

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,15 @@
-import { memo } from "react"
+import { memo } from "react";
 
 const Gallery = memo(function Gallery() {
   return (
-    (<div
-      className="flex-shrink-0 w-screen snap-start bg-white text-slate-950 overflow-y-auto">
-    </div>)
+    <div className="flex-shrink-0 w-screen snap-start bg-white text-slate-950 overflow-y-auto">
+      <div className="flex justify-center items-start pt-20">
+        <h1 className="text-5xl font-bold text-[#533C3C] font-inter leading-tight">
+          Gallery
+        </h1>
+      </div>
+    </div>
   );
-})
+});
 
-export default Gallery
-
+export default Gallery;
