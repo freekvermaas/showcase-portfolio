664d65cf9456fe13d24082dc4799f5ab285b1f8f
freekvermaas <freekvermaas@gmail.com>
2025-01-05 02:01:28 +0100

    gallery pagina positionering opnieuw met fix
    dankjewel yusuf voor de hulp
    ik heb ook de footer toegevoegd en een y scroll aangemaakt zodat je dat in ieder geval nu kan doen stel dat het niet uitkomt met de andere scroll optie die ik wil maken

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,120 +1,93 @@
-'use client'
+'use client';
 
-import { memo, useState, useEffect } from "react";
+import { memo } from "react";
 import Image from "next/image";
-import Link from "next/link";
+import Footer from '../components/Footer';
 
 const Gallery = memo(function Gallery() {
-  const [hoveredImage, setHoveredImage] = useState(null);
-  const [canScrollHorizontally, setCanScrollHorizontally] = useState(false);
-  const [isVerticalScroll, setIsVerticalScroll] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0 && !isVerticalScroll) {
-        setCanScrollHorizontally(true);
-      } else {
-        setCanScrollHorizontally(false);
-      }
-
-      if (window.scrollY > 0) {
-        setIsVerticalScroll(true);
-      } else {
-        setIsVerticalScroll(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [isVerticalScroll]);
-
   return (
-    <div className="w-screen h-screen snap-start bg-white text-slate-950 overflow-hidden flex flex-col">
+    <div className="w-screen h-screen text-slate-950 flex flex-col overflow-y-auto">
       <div className="flex justify-center pt-20">
         <h1 className="text-5xl font-bold text-[#533C3C] font-inter leading-tight">
           Gallery
         </h1>
       </div>
-
-      <div
-        className={`flex-grow flex flex-col justify-start items-center pt-10 p-8 overflow-y-auto ${canScrollHorizontally ? 'overflow-x-auto' : 'overflow-x-hidden'}`}
-      >
-        <div className="grid grid-cols-3 gap-4">
-          {/* Fotografie */}
-          <div className="col-span-2 row-span-2 bg-gray-300 flex items-center justify-center">
-            <p className="text-white text-xl font-bold">Fotografie</p>
-          </div>
-
-          {/* Upendo */}
-          <Link href="/Upendo">
-            <div
-              className="relative transition-transform duration-300 ease-in-out hover:scale-105 bg-gray-300 flex items-center justify-center w-[300px] h-[300px]"
-              onMouseEnter={() => setHoveredImage(1)}
-              onMouseLeave={() => setHoveredImage(null)}
-            >
+      <div className="flex h-[100%] justify-center mb-20">
+        <div className="flex w-[75%]">
+          <div className="flex flex-col items-end w-1/3 space-y-5 ml-20">
+            <div className="mt-5">
               <Image
-                src={hoveredImage === 1 ? "/images/post_upendo_showcase.png" : "/images/pre_upendo_showcase.png"}
+                src="/images/pre_tbd_showcase.png"
+                alt="Question mark Showcase"
+                width={250}
+                height={400}
+                className="object-cover"
+              />
+            </div>
+            <div>
+              <Image
+                src="/images/pre_fotografie_showcase.png"
+                alt="Fotografie Showcase"
+                width={250}
+                height={400}
+                className="object-cover"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col items-center w-1/3 space-y-5">
+            <div className="mt-5">
+              <Image
+                src="/images/pre_upendo_showcase.png"
                 alt="Upendo Showcase"
-                width={300}
-                height={300}
-                className="object-cover w-full h-full"
+                width={250}
+                height={250}
+                className="object-cover"
               />
             </div>
-          </Link>
-
-          {/* Video Editing */}
-          <div className="bg-gray-300 flex items-center justify-center w-[300px] h-[300px]">
-            <p className="text-white text-xl font-bold">Video Editing</p>
-          </div>
-
-          {/* Extra */}
-          <div className="bg-gray-300 flex items-center justify-center w-[300px] h-[300px]">
-            <p className="text-white text-xl font-bold">Extra</p>
-          </div>
-
-          {/* Trend Focus */}
-          <Link href="/TrendFocus">
-            <div
-              className="relative transition-transform duration-300 ease-in-out hover:scale-105 bg-gray-300 flex items-center justify-center w-[300px] h-[300px]"
-              onMouseEnter={() => setHoveredImage(2)}
-              onMouseLeave={() => setHoveredImage(null)}
-            >
+            <div>
               <Image
-                src={hoveredImage === 2 ? "/images/post_trendfocus_showcase.png" : "/images/pre_trendfocus_showcase.png"}
-                alt="Trend Focus Showcase"
-                width={300}
-                height={300}
-                className="object-cover w-full h-full"
+                src="/images/pre_trendfocus_showcase.png"
+                alt="TrendFocus Showcase"
+                width={250}
+                height={250}
+                className="object-cover"
               />
             </div>
-          </Link>
-
-          {/* Question mark */}
-          <div className="bg-gray-300 flex items-center justify-center w-[300px] h-[300px]">
-            <p className="text-white text-xl font-bold">?</p>
-          </div>
-
-          {/* McGoal */}
-          <Link href="/McGoal">
-            <div
-              className="relative transition-transform duration-300 ease-in-out hover:scale-105 bg-gray-300 flex items-center justify-center w-[300px] h-[300px]"
-              onMouseEnter={() => setHoveredImage(3)}
-              onMouseLeave={() => setHoveredImage(null)}
-            >
+            <div>
               <Image
-                src={hoveredImage === 3 ? "/images/post_mcgoal_showcase.png" : "/images/pre_mcgoal_showcase.png"}
+                src="/images/pre_mcgoal_showcase.png"
                 alt="McGoal Showcase"
-                width={300}
-                height={300}
-                className="object-cover w-full h-full"
+                width={250}
+                height={250}
+                className="object-cover"
               />
             </div>
-          </Link>
+          </div>
+          <div className="flex flex-col items-start w-1/3 space-y-5 mr-20">
+            <div className="mt-5">
+              <Image
+                src="/images/pre_videoediting_showcase.png"
+                alt="Video editing Showcase"
+                width={250}
+                height={400}
+                className="object-cover"
+              />
+            </div>
+            <div>
+              <Image
+                src="/images/pre_extra_showcase.png"
+                alt="Extra Showcase"
+                width={250}
+                height={400}
+                className="object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 });
 
 export default Gallery;
-
