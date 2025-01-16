d410bd203fb55bb6d0777a756abe39fbfc774309
freekvermaas <freekvermaas@gmail.com>
2025-01-16 14:23:37 +0100

    worldofpocs doc fix

--- src/app/worldofpocs/page.jsx
+++ src/app/worldofpocs/page.jsx
@@ -3,11 +3,26 @@
 import React from 'react';
 import GalleryHeader from '../../components/GalleryHeader';
 import Footer from '../../components/Footer';
+import Image from 'next/image';
 
 export default function worldofpocs() {
   return (
     <div className="flex flex-col min-h-screen">
-      <GalleryHeader/>
+      <GalleryHeader />
+      <div className="flex-grow flex flex-col items-center justify-start px-6 py-24 mt-16">
+        <div className="max-w-4xl w-full space-y-2">
+
+          {/* Intro Section */}
+          <div className="mb-16">
+            <h1 className="text-4xl md:text-5xl font-bold font-inter text-center mb-4">
+              WORLD OF POC'S
+            </h1>
+            <p className="text-base md:text-lg font-inter font-extralight text-center">
+            TBD.
+            </p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
